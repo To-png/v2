e178ea7ae785c4a307a28af17a6e8556dc85f9a5
Lik <to223344lik@gmail.com>
2021-09-12 15:54:02 +0300

    Pages

--- src/pages/Post4.vue
+++ src/pages/Post4.vue
@@ -1,13 +1,32 @@
 <template>
-  <h1>Пост 4, Запись 4</h1>
+  <a-layout class='layout'>
+    <SideBar />
+    <a-layout>
+      <HeaderPage />
+      <Post1Content />
+      <FooterPage />
+    </a-layout>
+  </a-layout>
 </template>
 
 <script>
-export default {
+  import SideBar from '@/UI/SideBar';
+  import HeaderPage from '@/UI/HeaderPage';
+  import Post1Content from '@/content/Post1Content.vue';
+  import FooterPage from '@/UI/FooterPage';
 
-}
+  export default {    
+    components: {
+      SideBar,        
+      HeaderPage,
+      Post1Content,
+      FooterPage
+    }
+  };
 </script>
 
 <style>
-
+  .layout {
+    min-height: 100vh
+  }
 </style>
--- src/pages/Stat2.vue
+++ src/pages/Stat2.vue
@@ -1,13 +1,32 @@
 <template>
-  <h1>Пост 2, Статистика 2</h1>
+  <a-layout class='layout'>
+    <SideBar />
+    <a-layout>
+      <HeaderPage />
+      <Post1Stat />
+      <FooterPage />
+    </a-layout>
+  </a-layout>
 </template>
 
 <script>
-export default {
+  import SideBar from '@/UI/SideBar';
+  import HeaderPage from '@/UI/HeaderPage'; 
+  import Post1Stat from '@/statistic/Post1Stat.vue'; 
+  import FooterPage from '@/UI/FooterPage';  
 
-}
+  export default {    
+    components: {
+      SideBar,        
+      HeaderPage, 
+      Post1Stat,     
+      FooterPage      
+    }
+  };
 </script>
 
 <style>
-
+  .layout {
+    min-height: 100vh
+  }
 </style>
